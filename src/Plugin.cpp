d899d01fec47c72babd926e2d579b0f5cdfb060e
fyryNy <fyryny@gmail.com>
2024-11-25 11:08:39 +0100

    Updated gothic and union api and multiplatform fix

--- src/Plugin.cpp
+++ src/Plugin.cpp
@@ -10,7 +10,6 @@
 #define ENGINE Engine_G1
 HOOKSPACE(Gothic_I_Classic, GetGameVersion() == ENGINE);
 #include "oCMsgAI.hpp"
-#include "Plugin_G1.hpp"
 #include "Plugin.hpp"
 #include "zCParser.hpp"
 #endif
@@ -19,24 +18,27 @@ HOOKSPACE(Gothic_I_Classic, GetGameVersion() == ENGINE);
 #define GOTHIC_NAMESPACE Gothic_I_Addon
 #define ENGINE Engine_G1A
 HOOKSPACE(Gothic_I_Addon, GetGameVersion() == ENGINE);
+#include "oCMsgAI.hpp"
 #include "Plugin.hpp"
-#include "Plugin_G1A.hpp"
+#include "zCParser.hpp"
 #endif
 
 #ifdef __G2
 #define GOTHIC_NAMESPACE Gothic_II_Classic
 #define ENGINE Engine_G2
 HOOKSPACE(Gothic_II_Classic, GetGameVersion() == ENGINE);
+#include "oCMsgAI.hpp"
 #include "Plugin.hpp"
-#include "Plugin_G2.hpp"
+#include "zCParser.hpp"
 #endif
 
 #ifdef __G2A
 #define GOTHIC_NAMESPACE Gothic_II_Addon
 #define ENGINE Engine_G2A
 HOOKSPACE(Gothic_II_Addon, GetGameVersion() == ENGINE);
+#include "oCMsgAI.hpp"
 #include "Plugin.hpp"
-#include "Plugin_G2A.hpp"
+#include "zCParser.hpp"
 #endif
 
 HOOKSPACE(Global, true);
